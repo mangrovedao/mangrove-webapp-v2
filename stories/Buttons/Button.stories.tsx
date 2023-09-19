7a72b239f95503354378f9a0bd763f54bd9af4cd
belbazanas <belbazanas@gmail.com>
2023-09-19 16:32:06 +0200

    add storybook
    Signed-off-by: belbazanas <belbazanas@gmail.com>

---
+++ stories/Buttons/Button.stories.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button"
+import type { Meta, StoryObj } from "@storybook/react"
+import { ArrowBigDown } from "lucide-react"
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: "Example/Button",
+  component: Button,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+  argTypes: {
+    disabled: {
+      control: { type: "boolean" },
+    },
+  },
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+} satisfies Meta<typeof Button>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default: Story = {
+  render: (args) => <Button {...args}>Button</Button>,
+}
+
+export const Icon: Story = {
+  render: (args) => (
+    <Button {...args} size={"icon"}>
+      <ArrowBigDown />
+    </Button>
+  ),
+}
