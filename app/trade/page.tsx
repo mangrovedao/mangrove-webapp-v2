614a864010b49e9edb1d95d51456619e83412d9b
Johan le Roch <johanleroch@gmail.com>
2023-11-14 18:13:20 +0100

    wip

--- app/trade/page.tsx
+++ app/trade/page.tsx
@@ -5,7 +5,7 @@ import Trade from "./sections/trade/trade"
 
 export default function Page() {
   return (
-    <div className="p-2 h-full">
+    <div className="py-2 h-full">
       <Header />
       <div className="grid grid-cols-4 gap-3 h-full">
         <div className="sm:col-span-4 md:col-span-2 lg:col-span-1 border border-solid border-muted rounded-md">
