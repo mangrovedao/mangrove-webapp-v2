289e48f37967f7ca2bdc83823e8eff2d73dfb6c1
Johan le Roch <johanleroch@gmail.com>
2023-11-15 20:57:59 +0100

    wip

--- app/trade/components/market-infos-bar.tsx
+++ app/trade/components/market-infos-bar.tsx
@@ -1,10 +1,10 @@
 "use client"
-
 import { Separator } from "@/components/ui/separator"
 import { Skeleton } from "@/components/ui/skeleton"
 import useTokenPriceQuery from "@/hooks/use-token-price-query"
 import useMarket from "@/providers/market"
 import { formatNumber } from "@/utils/numbers"
+import React from "react"
 
 function Container({ children }: React.PropsWithChildren) {
   return <span className="text-xs font-medium space-y-[2px]">{children}</span>
@@ -23,11 +23,13 @@ function Item({
   value,
   skeleton = true,
   decimals = 2,
+  showSymbol = false,
 }: {
   label: string
   value?: number | bigint
   skeleton?: boolean
   decimals?: number
+  showSymbol?: boolean
 }) {
   return (
     <Container>
@@ -35,9 +37,9 @@ function Item({
       {!skeleton ? (
         <Value>
           {formatNumber(value ?? 0, {
-            style: "currency",
-            currencyDisplay: "symbol",
-            currency: "USD",
+            style: showSymbol ? "currency" : undefined,
+            currencyDisplay: showSymbol ? "symbol" : undefined,
+            currency: showSymbol ? "USD" : undefined,
             minimumFractionDigits: decimals,
             maximumFractionDigits: decimals,
           })}
@@ -49,58 +51,56 @@ function Item({
   )
 }
 
+const keyLabels = {
+  high: "24h High",
+  low: "24h Low",
+}
+
 export default function MarketInfosBar() {
   const { selectedMarket } = useMarket()
   const oneMinutePriceQuery = useTokenPriceQuery(
     selectedMarket?.base.name,
     selectedMarket?.quote.name,
   )
-  const displayedDecimals = selectedMarket?.quote.displayedDecimals || 2
-
-  const props = {
-    value: oneMinutePriceQuery?.data?.close,
-    decimals: displayedDecimals,
-    skeleton: oneMinutePriceQuery?.isLoading,
-  }
+  const oneDayPriceQuery = useTokenPriceQuery(
+    selectedMarket?.base.name,
+    selectedMarket?.quote.name,
+    "1d",
+  )
+  const displayedDecimals = selectedMarket?.quote.displayedDecimals
+  const quoteName = selectedMarket?.quote.name
 
   return (
     <div className="flex items-center w-full space-x-8 whitespace-nowrap h-full">
-      <Item label="Price" {...props} />
+      <Item
+        label="Price"
+        value={oneMinutePriceQuery?.data?.close}
+        decimals={displayedDecimals}
+        skeleton={oneMinutePriceQuery?.isLoading}
+        showSymbol
+      />
+
+      {Object.entries(keyLabels).map(([key, label]) => (
+        <React.Fragment key={key}>
+          <Separator orientation="vertical" className="h-4" />
+
+          <Item
+            label={label}
+            value={oneDayPriceQuery?.data?.[key as keyof typeof keyLabels]}
+            decimals={displayedDecimals}
+            skeleton={oneDayPriceQuery?.isLoading}
+          />
+        </React.Fragment>
+      ))}
 
       <Separator orientation="vertical" className="h-4" />
 
-      <Container>
-        <Label>24h change</Label>
-        <Value>21.36 +0.2%</Value>
-      </Container>
-
-      <Separator orientation="vertical" className="h-4" />
-
-      <Container>
-        <Label>24h high</Label>
-        <Value>1 234.12</Value>
-      </Container>
-
-      <Separator orientation="vertical" className="h-4" />
-
-      <Container>
-        <Label>24h low</Label>
-        <Value>1 234.12</Value>
-      </Container>
-
-      <Separator orientation="vertical" className="h-4" />
-
-      <Container>
-        <Label>24h volume (ETH)</Label>
-        <Value>123 456.12</Value>
-      </Container>
-
-      <Separator orientation="vertical" className="h-4" />
-
-      <Container>
-        <Label>24h volume (USDC)</Label>
-        <Value>123 456.12</Value>
-      </Container>
+      <Item
+        label={`24h Volume (${quoteName})`}
+        value={oneDayPriceQuery?.data?.volume}
+        decimals={displayedDecimals}
+        skeleton={oneDayPriceQuery?.isLoading}
+      />
     </div>
   )
 }
--- schemas/cryptocompare-price-api.ts
+++ schemas/cryptocompare-price-api.ts
@@ -1,8 +1,5 @@
 import { z } from "zod"
-import {
-  mangrovePriceResponseSchema,
-  type MangrovePriceResponse,
-} from "./mangrove-price-api"
+import { mangrovePriceResponseSchema } from "./mangrove-price-api"
 
 export const cryptoComparePriceResponseSchema = z.record(z.number())
 
@@ -10,17 +7,24 @@ export type CryptoComparePriceResponse = z.infer<
   typeof cryptoComparePriceResponseSchema
 >
 
+const partialMangrovePriceResponseSchema = mangrovePriceResponseSchema.partial({
+  open: true,
+  high: true,
+  low: true,
+  date: true,
+  volume: true,
+})
+
+type PartialMangrovePriceResponseSchema = z.infer<
+  typeof partialMangrovePriceResponseSchema
+>
+
 export function convertToMangroveApiResponse(
   response: Record<string, number>,
-): MangrovePriceResponse {
+): PartialMangrovePriceResponseSchema {
   const tokenName = Object.keys(response)[0]
   if (!tokenName) throw new Error("Invalid response from CryptoCompare API")
-  return mangrovePriceResponseSchema.parse({
-    open: response[tokenName],
-    high: response[tokenName],
-    low: response[tokenName],
+  return mangrovePriceResponseSchema.partial({}).parse({
     close: response[tokenName],
-    volume: 0,
-    date: new Date().toISOString(),
   })
 }
