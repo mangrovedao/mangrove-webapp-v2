707681c3b3ceb150c1156835a78bebeb4900a7af
belbazanas <belbazanas@gmail.com>
2023-11-27 17:18:08 +0100

    add skeleton + toolbar backgroundcolor
    Signed-off-by: belbazanas <belbazanas@gmail.com>

--- components/trading-view/trading-view-chart.tsx
+++ components/trading-view/trading-view-chart.tsx
@@ -1,19 +1,24 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client"
 
+import { cn } from "@/utils"
 import React from "react"
 import {
   widget,
   type ChartingLibraryWidgetOptions,
 } from "../../public/charting_library"
+import { Skeleton } from "../ui/skeleton"
 
 export const TVChartContainer = (
   props: Partial<ChartingLibraryWidgetOptions>,
 ) => {
+  const [isLoading, setIsLoading] = React.useState(true)
   const chartContainerRef =
     React.useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>
 
   React.useEffect(() => {
+    console.log(chartContainerRef)
+
     const widgetOptions: ChartingLibraryWidgetOptions = {
       symbol: props.symbol,
       // BEWARE: no trailing slash is expected in feed URL
@@ -45,114 +50,38 @@ export const TVChartContainer = (
         "create_volume_indicator_by_default",
         "timezone_menu",
         "timeframes_toolbar",
+        "hide_main_series_symbol_from_indicator_legend",
       ],
       overrides: {
         "paneProperties.background": "#010e0e",
         "paneProperties.backgroundType": "solid",
       },
-      // isolatedModules: false,
-      // custom_indicators_getter: (PineJS) => {
-      //   return Promise.resolve<CustomIndicator[]>([
-      //     /* Coloring bars */
-      //     {
-      //       name: "Bar Colorer Demo",
-      //       metainfo: {
-      //         _metainfoVersion: 51,
-
-      //         id: "BarColoring@tv-basicstudies-1" as RawStudyMetaInfoId,
-      //         name: "BarColoring",
-      //         description: "Bar Colorer Demo",
-      //         shortDescription: "BarColoring",
-
-      //         isCustomIndicator: true,
-      //         is_price_study: true,
-
-      //         format: {
-      //           type: "price",
-      //           precision: 4,
-      //         },
-
-      //         defaults: {
-      //           palettes: {
-      //             palette_0: {
-      //               // palette colors
-      //               // change it to the default colors that you prefer,
-      //               // but note that the user can change them in the Style tab
-      //               // of indicator properties
-      //               colors: [{ color: "#FFFF00" }, { color: "#0000FF" }],
-      //             },
-      //           },
-      //         },
-      //         inputs: [],
-      //         plots: [
-      //           {
-      //             id: "plot_0",
-
-      //             // plot type should be set to 'bar_colorer'
-      //             type: StudyPlotType.BarColorer,
-
-      //             // this is the name of the palette that is defined
-      //             // in 'palettes' and 'defaults.palettes' sections
-      //             palette: "palette_0",
-      //           },
-      //         ],
-      //         palettes: {
-      //           palette_0: {
-      //             colors: [{ name: "Color 0" }, { name: "Color 1" }],
-
-      //             // the mapping between the values that
-      //             // are returned by the script and palette colors
-      //             valToIndex: {
-      //               100: 0,
-      //               200: 1,
-      //             },
-      //           },
-      //         },
-      //       },
-      //       constructor: function (this: LibraryPineStudy<IPineStudyResult>) {
-      //         this.main = function (context, input) {
-      //           this._context = context
-      //           this._input = input
-
-      //           const valueForColor0 = 100
-      //           const valueForColor1 = 200
-
-      //           // perform your calculations here and return one of the constants
-      //           // that is specified as a key in 'valToIndex' mapping
-      //           const result =
-      //             (Math.random() * 100) % 2 > 1 // we randomly select one of the color values
-      //               ? valueForColor0
-      //               : valueForColor1
-
-      //           return [result]
-      //         }
-      //       },
-      //     },
-      //   ])
-      // },
-      // enabled_features: ["study_templates"],
-      // charts_storage_url: props.charts_storage_url,
-      // charts_storage_api_version: props.charts_storage_api_version,
-      // client_id: props.client_id,
-      // user_id: props.user_id,
-      // autosize: props.autosize,
     }
 
     const tvWidget = new widget(widgetOptions)
+    const element = chartContainerRef.current.querySelector(
+      '[id^="tradingview"]',
+    )
+    if (!element) return
+    element.classList.add("w-full")
+    element.classList.add("h-full")
 
     tvWidget.onChartReady(() => {
-      tvWidget.headerReady().then(() => {
-        const element = chartContainerRef.current.querySelector(
-          '[id^="tradingview"]',
-        )
-        if (!element) return
-        element.classList.add("w-full")
-        element.classList.add("h-full")
-      })
+      setIsLoading(false)
     })
+
     return () => {
       tvWidget.remove()
     }
   }, [props])
-  return <div className="h-full" ref={chartContainerRef} />
+
+  return (
+    <div className="w-full h-full relative">
+      {isLoading && <Skeleton className="absolute inset-0" />}
+      <div
+        className={cn("h-full transition-opacity", { "opacity-0": isLoading })}
+        ref={chartContainerRef}
+      />
+    </div>
+  )
 }
