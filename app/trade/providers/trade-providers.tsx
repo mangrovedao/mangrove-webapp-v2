c196afcddc3d1446ae5b2c2c34132ffa8bdba9a3
Johan le Roch <johanleroch@gmail.com>
2023-12-16 10:17:02 +0100

    fix walletconnect issue

--- app/trade/providers/trade-providers.tsx
+++ app/trade/providers/trade-providers.tsx
@@ -3,7 +3,7 @@ import React from "react"
 import { IndexerSdkProvider } from "@/providers/mangrove-indexer"
 import { MarketProvider } from "@/providers/market"
 
-export default function Layout({ children }: React.PropsWithChildren) {
+export function TradeProviders({ children }: React.PropsWithChildren) {
   return (
     <MarketProvider>
       <IndexerSdkProvider>{children}</IndexerSdkProvider>
