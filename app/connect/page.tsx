753fd12661b4e909fd6305e164e0efe2665d199c
Johan le Roch <johanleroch@gmail.com>
2023-12-16 09:59:47 +0100

    test fix walletconnect

--- app/connect/page.tsx
+++ app/connect/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 
 import { ClientOnly } from "@/components/client-only"
+import { Button } from "@/components/ui/button"
 import {
   useWeb3Modal,
   useWeb3ModalEvents,
   useWeb3ModalState,
   useWeb3ModalTheme,
 } from "@web3modal/wagmi/react"
+import { useAccount } from "wagmi"
 
 export default function Page() {
   // 4. Use modal hook
@@ -14,6 +16,7 @@ export default function Page() {
   const state = useWeb3ModalState()
   const { themeMode, themeVariables, setThemeMode } = useWeb3ModalTheme()
   const events = useWeb3ModalEvents()
+  const { isConnected, connector, address, isConnecting } = useAccount()
 
   return (
     <main className="w-full items-center flex flex-col">
@@ -22,16 +25,17 @@ export default function Page() {
         <w3m-network-button />
         <w3m-connect-button />
         <w3m-account-button />
-
-        <button onClick={() => modal.open()}>Connect Wallet</button>
-        <button onClick={() => modal.open({ view: "Networks" })}>
+        <Button onClick={() => modal.open({ view: "Connect" })}>
+          Connect Wallet
+        </Button>
+        <Button onClick={() => modal.open({ view: "Networks" })}>
           Choose Network
-        </button>
-        <button
+        </Button>
+        <Button
           onClick={() => setThemeMode(themeMode === "dark" ? "light" : "dark")}
         >
           Toggle Theme Mode
-        </button>
+        </Button>
         <pre>{JSON.stringify(state, null, 2)}</pre>
         <pre>{JSON.stringify({ themeMode, themeVariables }, null, 2)}</pre>
         <pre>{JSON.stringify(events, null, 2)}</pre>
