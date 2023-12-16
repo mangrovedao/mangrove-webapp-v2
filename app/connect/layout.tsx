e2e82b33b87788a3f7081f9d7c21d8a7e617f980
Johan le Roch <johanleroch@gmail.com>
2023-12-16 10:47:14 +0100

    trying to identify which contexts makes walletconnect not working

--- app/connect/layout.tsx
+++ app/connect/layout.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 
-import { IndexerSdkProvider } from "@/providers/mangrove-indexer"
 import { MarketProvider } from "@/providers/market"
 
 export default function Layout({ children }: React.PropsWithChildren) {
   return (
     <MarketProvider>
-      <IndexerSdkProvider>{children}</IndexerSdkProvider>
+      {children}
+      {/* <IndexerSdkProvider>{children}</IndexerSdkProvider> */}
     </MarketProvider>
   )
 }
