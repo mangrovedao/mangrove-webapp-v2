f7059551faca475f1cd2982748cefa00934ebac7
Johan le Roch <johanleroch@gmail.com>
2023-12-20 16:40:13 +0100

    pagination styling

--- components/ui/data-table/pagination.tsx
+++ components/ui/data-table/pagination.tsx
@@ -1,49 +1,53 @@
 import * as Ark from "@ark-ui/react"
+import { ChevronLeft, ChevronRight } from "lucide-react"
 
 export type PaginationProps = typeof Ark.Pagination.Root.defaultProps
 
 const buttonClasses =
-  "px-2 py-4 text-sm hover:text-white transition-colors disabled:text-gray-scale-400 aria-[current=page]:text-white"
+  "text-sm hover:text-white transition-colors disabled:text-gray-scale-400 aria-[current=page]:text-white aria-[current=page]:border aria-[current=page]:border-green-caribbean rounded-full h-8 w-8 flex justify-center items-center"
+const ellipsisClasses =
+  "h-8 w-8 flex justify-center items-center cursor-default"
 
-// TODO: implement count once ready on the indexer
 export function Pagination(props: PaginationProps) {
   return (
-    <Ark.Pagination.Root
-      count={100}
-      pageSize={10}
-      siblingCount={0}
-      {...props}
-      className="w-full flex justify-between text-gray-scale-300 mb-2"
-    >
-      {({ pages }) => (
-        <>
-          <Ark.Pagination.PrevTrigger className={buttonClasses}>
-            Previous
-          </Ark.Pagination.PrevTrigger>
-          {pages.map((page, index) =>
-            page.type === "page" ? (
-              <Ark.Pagination.Item
-                key={index}
-                {...page}
-                className={buttonClasses}
-              >
-                {page.value}
-              </Ark.Pagination.Item>
-            ) : (
-              <Ark.Pagination.Ellipsis
-                key={index}
-                index={index}
-                className={buttonClasses}
-              >
-                &#8230;
-              </Ark.Pagination.Ellipsis>
-            ),
-          )}
-          <Ark.Pagination.NextTrigger className={buttonClasses}>
-            Next
-          </Ark.Pagination.NextTrigger>
-        </>
-      )}
-    </Ark.Pagination.Root>
+    <div className="flex w-full justify-center">
+      <Ark.Pagination.Root
+        count={100}
+        pageSize={10}
+        siblingCount={0}
+        {...props}
+        className="justify-between text-gray-scale-300 mb-2 bg-primary-solid-black rounded-full text-sm font-medium p-3 inline-flex gap-4"
+      >
+        {({ pages }) => (
+          <>
+            <Ark.Pagination.PrevTrigger className={buttonClasses}>
+              <ChevronLeft />
+            </Ark.Pagination.PrevTrigger>
+            {pages.map((page, index) =>
+              page.type === "page" ? (
+                <Ark.Pagination.Item
+                  key={index}
+                  {...page}
+                  className={buttonClasses}
+                >
+                  {page.value}
+                </Ark.Pagination.Item>
+              ) : (
+                <Ark.Pagination.Ellipsis
+                  key={index}
+                  index={index}
+                  className={ellipsisClasses}
+                >
+                  &#8230;
+                </Ark.Pagination.Ellipsis>
+              ),
+            )}
+            <Ark.Pagination.NextTrigger className={buttonClasses}>
+              <ChevronRight />
+            </Ark.Pagination.NextTrigger>
+          </>
+        )}
+      </Ark.Pagination.Root>
+    </div>
   )
 }
