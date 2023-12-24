10ed56580c17f070dd2f36f4527cbf55097de761
Johan le Roch <johanleroch@gmail.com>
2023-12-24 15:01:26 +0100

    new strat layout improvement

--- app/strategies/new/page.tsx
+++ app/strategies/new/page.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
 import { Form } from "./_components/form/form"
 import { InfoBar } from "./_components/info-bar"
 
@@ -6,9 +9,10 @@ export default function Page() {
     <div className="grid grid-rows-[auto,1fr] h-[calc(100vh-var(--bar-height))] w-full">
       <InfoBar />
       <div className="grid grid-cols-[25.375rem,1fr] max-w-8xl mx-auto w-full overflow-auto">
-        <div className="overflow-auto p-6 border-r border-l">
-          <Form />
-        </div>
+        <ScrollArea>
+          <Form className="p-6 border-r border-l" />
+          <ScrollBar orientation="vertical" />
+        </ScrollArea>
         <div className="overflow-auto p-6 border-r">
           {/* Content of the right column goes here */}
           <h1>Content</h1>
