ad78ff0ba60430710f6d480bf649cc595c6cb21d
Johan le Roch <johanleroch@gmail.com>
2023-12-27 18:15:43 +0100

    set range animation

--- app/strategies/new/_components/price-range/components/price-chart/set-range-animation.tsx
+++ app/strategies/new/_components/price-range/components/price-chart/set-range-animation.tsx
@@ -1,65 +1,74 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useGSAP } from "@gsap/react"
+import { gsap } from "gsap"
 import { MousePointer, MousePointerClick } from "lucide-react"
 import React from "react"
 
 export function SetRangeAnimation() {
-  const [stage, setStage] = React.useState(0)
+  const pointerRef = React.useRef(null)
+  const clickRef = React.useRef(null)
+  const containerRef = React.useRef<HTMLDivElement | null>(null)
 
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      setStage((prevStage) => (prevStage + 1) % 3)
-    }, 1000) // Change stage every 1 second
-
-    return () => {
-      clearInterval(interval)
-    }
-  }, [])
+  useGSAP(
+    () => {
+      if (!containerRef.current) return
+      gsap
+        .timeline({
+          repeat: -1,
+        })
+        .from(clickRef.current, {
+          display: "none",
+        })
+        .from(
+          pointerRef.current,
+          {
+            opacity: 0,
+          },
+          "<",
+        )
+        .to(
+          pointerRef.current,
+          {
+            opacity: 1,
+          },
+          "<",
+        )
+        .to(
+          pointerRef.current,
+          {
+            display: "none",
+          },
+          "<",
+        )
+        .to(clickRef.current, {
+          display: "block",
+          opacity: 1,
+        })
+        .to(
+          clickRef.current,
+          {
+            x: containerRef.current?.offsetWidth / 2,
+            duration: 2.5,
+            delay: 0.3,
+            ease: "Expo.out",
+          },
+          "<",
+        )
+        .to(clickRef.current, {
+          delay: 0.3,
+          opacity: 0,
+        })
+        .play()
+    },
+    {
+      scope: containerRef,
+    },
+  )
 
   return (
-    <div className={`cursor-animation stage-${stage}`}>
-      <MousePointer className="mouse-pointer" />
-      <MousePointerClick className="mouse-pointer-click" />
-      <style global jsx>{`
-        .cursor-animation {
-          position: relative;
-          animation: moveRight 3s ease-in-out infinite;
-        }
-
-        .mouse-pointer,
-        .mouse-pointer-click {
-          position: absolute;
-          transition: opacity 0.5s;
-        }
-
-        .mouse-pointer {
-          opacity: 1;
-        }
-
-        .mouse-pointer-click {
-          opacity: 0;
-        }
-
-        .cursor-animation.stage-1 .mouse-pointer,
-        .cursor-animation.stage-2 .mouse-pointer-click {
-          opacity: 0;
-        }
-
-        .cursor-animation.stage-1 .mouse-pointer-click,
-        .cursor-animation.stage-2 .mouse-pointer {
-          opacity: 1;
-        }
-
-        @keyframes moveRight {
-          0% {
-            transform: translateX(0);
-          }
-          33% {
-            transform: translateX(0);
-          }
-          100% {
-            transform: translateX(50%); /* Adjust as needed */
-          }
-        }
-      `}</style>
+    <div className="cursor-animation" ref={containerRef}>
+      <MousePointer ref={pointerRef} />
+      <MousePointerClick ref={clickRef} className="hidden" />
     </div>
   )
 }
