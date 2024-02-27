57b159faaf382efb8ec6a5622856bfa928e15042
belbazanas <belbazanas@gmail.com>
2024-02-27 12:01:50 +0100

    remove unused code
    Signed-off-by: belbazanas <belbazanas@gmail.com>

--- app/trade/_components/forms/amplified/components/from-wallet-order-dialog.tsx
+++ app/trade/_components/forms/amplified/components/from-wallet-order-dialog.tsx
@@ -180,6 +180,7 @@ export default function FromWalletAmplifiedOrderDialog({
                 form,
               },
               {
+                onSettled: onClose,
                 onError: (error: Error) => {
                   onClose()
                   tradeService.openTxFailedDialog(
