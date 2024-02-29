84311f56a3f358dc73c4ab8c655bd3974cf8afd0
Johan le Roch <johanleroch@gmail.com>
2024-02-29 19:30:36 +0100

    set blast mainnet config

--- providers/wallet-connect.tsx
+++ providers/wallet-connect.tsx
@@ -3,6 +3,7 @@
 import "@rainbow-me/rainbowkit/styles.css"
 
 import {
+  Chain,
   RainbowKitProvider,
   darkTheme,
   getDefaultConfig,
@@ -17,16 +18,38 @@ import { getWhitelistedChainObjects } from "@/utils/chains"
 const queryClient = new QueryClient()
 const projectId = env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID
 
+const blastMainnetRpcUrl = process.env.NEXT_PUBLIC_BLAST_MAINNET_RPC_URL ?? ""
+
+const blastMainner = {
+  id: 81457,
+  name: "Blast",
+  iconUrl:
+    "https://cdn.routescan.io/_next/image?url=https%3A%2F%2Fcms-cdn.avascan.com%2Fcms2%2Fblast.dead36673539.png&w=48&q=100",
+  nativeCurrency: {
+    name: "Ethereum",
+    symbol: "ETH",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: [blastMainnetRpcUrl],
+    },
+  },
+} as const satisfies Chain
+
 const config = getDefaultConfig({
   appName: "Mangrove dApp",
   projectId,
   // @ts-ignore
-  chains: getWhitelistedChainObjects(),
+  chains: blastMainnetRpcUrl
+    ? [...getWhitelistedChainObjects(), blastMainner]
+    : getWhitelistedChainObjects(),
   transports: {
     [wagmiChains.polygon.id]: http(),
     [wagmiChains.polygonMumbai.id]: http(),
     [wagmiChains.blastSepolia.id]: http(),
     [wagmiChains.arbitrum.id]: http(),
+    [81457]: http(),
   },
   ssr: true,
 })
