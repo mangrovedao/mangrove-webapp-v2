6d59e02a50c67e1a6c289388adc06acd6beb97c3
Johan le Roch <johanleroch@gmail.com>
2024-02-29 12:29:36 +0100

    additionnal config

--- next.config.mjs
+++ next.config.mjs
@@ -34,6 +34,10 @@ const config = {
       },
     ])
   },
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding")
+    return config
+  },
 }
 
 export default withSentryConfig(
