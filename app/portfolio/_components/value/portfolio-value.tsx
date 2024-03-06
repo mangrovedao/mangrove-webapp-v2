bf744d0614e8ed225f6f218841cb799bda7dfe5e
Sasi <hey@sasi.codes>
2024-03-06 18:41:06 +0530

    Add circular progress bar and additional data in PortfolioValue component

--- app/portfolio/_components/value/portfolio-value.tsx
+++ app/portfolio/_components/value/portfolio-value.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { CircularProgressBar } from "@/components/ui/circle-progress-bar"
 import DownArrowIcon from "../../_icons/DownArrowIcon"
 import UpArrowIcon from "../../_icons/UpArrowIcon"
 
@@ -22,6 +23,46 @@ export default function PortfolioValue() {
               <DownArrowIcon />
             </span>
             <span>3%</span>
+          </div>
+          <span className="opacity-70 pl-4">Past week</span>
+        </div>
+
+        <div className="bg-primary-bush-green items-center rounded-xl p-6 flex">
+          <div className="pr-10">
+            <CircularProgressBar
+              size={75}
+              strokeWidth={20}
+              progress={75}
+              className="ml-3"
+            />
+          </div>
+
+          <div className="flex flex-col flex-1">
+            <div className="flex justify-between items-center">
+              <div className="flex items-center space-x-2">
+                <div className="bg-green-caribbean size-2.5 rounded-full" />
+                <span>Orders</span>
+                <span className="bg-primary-dark-green py-1 px-2 text-cloud-200 rounded-lg">
+                  3
+                </span>
+              </div>
+              <span>
+                $ 1234.12 <span className="opacity-60">(75%)</span>
+              </span>
+            </div>
+
+            <div className="flex justify-between items-center">
+              <div className="flex items-center space-x-2">
+                <div className="bg-green-bangladesh size-2.5 rounded-full" />
+                <span>Strategies</span>
+                <span className="bg-primary-dark-green py-1 px-2 text-cloud-200 rounded-lg">
+                  5
+                </span>
+              </div>
+              <span>
+                $ 640.60 <span className="opacity-60">(25%)</span>
+              </span>
+            </div>
           </div>
         </div>
       </div>
