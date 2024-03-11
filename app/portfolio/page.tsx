48562e6959b8bae56e2d5a9af288420fd315fef1
Sasi <hey@sasi.codes>
2024-03-11 14:16:47 +0530

    Add scroll functionality to portfolio page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -5,27 +5,31 @@ import OverviewCharts from "./_components/value/charts"
 import Metrics from "./_components/value/metrics"
 import OpenOrders from "./_components/value/open-orders"
 import PortfolioValue from "./_components/value/portfolio-value"
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
 
 export default function Page() {
   return (
-    <main className="w-full">
-      <div className="lg:grid w-full grid-cols-3">
-        <section className="border-r h-96 col-span-1">
-          <PortfolioValue />
-        </section>
-        <section className="col-span-2">
-          <OverviewCharts />
-        </section>
-        <div className="border-y py-8 col-span-3">
-          <Metrics />
+    <ScrollArea className="h-screen pb-6" scrollHideDelay={200}>
+      <main className="w-full">
+        <div className="lg:grid w-full grid-cols-3">
+          <section className="border-r h-96 col-span-1">
+            <PortfolioValue />
+          </section>
+          <section className="col-span-2">
+            <OverviewCharts />
+          </section>
+          <div className="border-y py-8 col-span-3">
+            <Metrics />
+          </div>
+          <div className="py-8 col-span-3">
+            <Balance />
+          </div>
+          <div className="py-8 col-span-3">
+            <OpenOrders />
+          </div>
         </div>
-        <div className="py-8 col-span-3">
-          <Balance />
-        </div>
-        <div className="py-8 col-span-3">
-          <OpenOrders />
-        </div>
-      </div>
-    </main>
+      </main>
+      <ScrollBar orientation="vertical" className="z-50" />
+    </ScrollArea>
   )
 }
