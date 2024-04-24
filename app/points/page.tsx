03b594be24bb789f0edb21fe8b22c23eed57e416
Johan le Roch <johanleroch@gmail.com>
2024-04-24 15:45:21 +0200

    disable leaderboard

--- app/points/page.tsx
+++ app/points/page.tsx
@@ -2,10 +2,13 @@
 
 import { useAccount } from "wagmi"
 
+import { Caption } from "@/components/typography/caption"
+import { cn } from "@/utils"
+import { Settings, X } from "lucide-react"
+import { useLocalStorage } from "usehooks-ts"
 import { ConnectWalletBanner } from "./_components/connect-wallet-banner"
 import CurrentBoost from "./_components/current-boost"
 import { JoinProgramBanner } from "./_components/join-program-banner"
-import { Leaderboard } from "./_components/leaderboard/table"
 import {
   useUserBoosts,
   useUserPoints,
@@ -35,6 +38,7 @@ export default function Page() {
 
   return isConnected ? (
     <div>
+      <LeaderboardWarningBanner />
       <JoinProgramBanner />
       <div className="flex flex-col items-center md:flex-row md:items-start md:justify-between">
         <Rewards />
@@ -55,11 +59,53 @@ export default function Page() {
           boost={userBoost?.boost}
         />
       </div>
-      <Leaderboard />
+      {/* <Leaderboard /> */}
     </div>
   ) : (
     <div>
       <ConnectWalletBanner />
     </div>
+  )
+}
+
+function LeaderboardWarningBanner() {
+  const [leaderboardWarning, setLeaderboardWarning] = useLocalStorage<
+    boolean | null
+  >("leaderboardWarning", null)
+
+  if (leaderboardWarning) return null
+  return (
+    <aside
+      className={cn(
+        "w-full flex justify-between space-x-4 align-middle rounded-lg mt-2 px-3 py-2 bg-mango-300 mb-6",
+      )}
+    >
+      <div className="flex align-middle items-center space-x-2">
+        <div
+          className={cn(
+            "h-8 aspect-square rounded-lg flex items-center justify-center text-red-100 p-1 bg-mango-300",
+          )}
+        >
+          <Settings className={"text-mango-100"} />
+        </div>
+        <div>
+          <Caption>
+            Please note:{" "}
+            <i>
+              The leaderboard is currently reaggregating data. <br />
+              We appreciate your patience and understanding as our developers
+              work diligently on these enhancements.
+            </i>
+          </Caption>
+        </div>
+      </div>
+      <button
+        className="top-3 right-2 hover:opacity-90 transition-opacity"
+        onClick={() => setLeaderboardWarning(true)}
+      >
+        <X className="text-cloud-300 w-5 h-5 hover:text-secondary" />
+        <span className="sr-only">Close</span>
+      </button>
+    </aside>
   )
 }
