e8db08e36c985228ad86ed3f67af5c42da016254
Daniel <16984675+DanielRX@users.noreply.github.com>
2024-05-13 12:48:13 +0100

    Use volume not points for MS1 banner

--- app/points/_components/leaderboard/use-leaderboard.ts
+++ app/points/_components/leaderboard/use-leaderboard.ts
@@ -4,9 +4,7 @@ import { keepPreviousData, useQuery } from "@tanstack/react-query"
 import { useAccount } from "wagmi"
 
 import { getErrorMessage } from "@/utils/errors"
-import { parseBoosts } from "../../schemas/boosts"
 import { parseLeaderboard } from "../../schemas/leaderboard"
-import { parsePoints } from "../../schemas/points"
 import { parseVolume } from "../../schemas/volume"
 
 type Params = {
@@ -39,58 +37,6 @@ export function useLeaderboard({
     retry: false,
     staleTime: 1 * 60 * 1000, // 1 minute
     placeholderData: keepPreviousData,
-  })
-}
-
-export function useUserPoints() {
-  const { address } = useAccount()
-  return useQuery({
-    queryKey: ["user-points", address],
-    queryFn: async () => {
-      try {
-        if (!address) return null
-        const res = await fetch(
-          `${process.env.NEXT_PUBLIC_MANGROVE_DATA_API_HOST}/incentives/points/${address}`,
-        )
-        const points = await res.json()
-        return parsePoints(points)
-      } catch (e) {
-        console.error(getErrorMessage(e))
-        throw new Error()
-      }
-    },
-    meta: {
-      error: "Unable to retrieve user rank data",
-    },
-    enabled: !!address,
-    retry: false,
-    staleTime: 1 * 60 * 1000, // 1 minute
-  })
-}
-
-export function useUserBoosts() {
-  const { address } = useAccount()
-  return useQuery({
-    queryKey: ["user-boosts", address],
-    queryFn: async () => {
-      try {
-        if (!address) return null
-        const res = await fetch(
-          `${process.env.NEXT_PUBLIC_MANGROVE_DATA_API_HOST}/incentives/boosts/${address}`,
-        )
-        const boosts = await res.json()
-        return parseBoosts(boosts)
-      } catch (e) {
-        console.error(getErrorMessage(e))
-        throw new Error()
-      }
-    },
-    meta: {
-      error: "Unable to retrieve user boosts data",
-    },
-    enabled: !!address,
-    retry: false,
-    staleTime: 1 * 60 * 1000, // 1 minute
   })
 }
 
