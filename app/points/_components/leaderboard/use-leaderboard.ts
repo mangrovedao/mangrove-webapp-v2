87c64c0d9ce12838ffba39e57beac85506793114
Anas <belbazanas@gmail.com>
2024-05-14 14:10:58 +0200

    Revert "Update volumes and boost to use new API (#254)" (#256)
    This reverts commit 64831ae664017b40796d76e6efbf42cfb29a878f.

--- app/points/_components/leaderboard/use-leaderboard.ts
+++ app/points/_components/leaderboard/use-leaderboard.ts
@@ -4,8 +4,9 @@ import { keepPreviousData, useQuery } from "@tanstack/react-query"
 import { useAccount } from "wagmi"
 
 import { getErrorMessage } from "@/utils/errors"
+import { parseBoosts } from "../../schemas/boosts"
 import { parseLeaderboard } from "../../schemas/leaderboard"
-import { parseVolume } from "../../schemas/volume"
+import { parsePoints } from "../../schemas/points"
 
 type Params = {
   filters?: {
@@ -40,25 +41,51 @@ export function useLeaderboard({
   })
 }
 
-export function useUserVolume() {
+export function useUserPoints() {
   const { address } = useAccount()
   return useQuery({
-    queryKey: ["user-volume", address],
+    queryKey: ["user-points", address],
     queryFn: async () => {
       try {
         if (!address) return null
         const res = await fetch(
-          `${process.env.NEXT_PUBLIC_MANGROVE_DATA_API_HOST}/volumes-per-pair/last-epoch-volume/${address}`,
+          `${process.env.NEXT_PUBLIC_MANGROVE_DATA_API_HOST}/incentives/points/${address}`,
         )
-        const volume = await res.json()
-        return parseVolume(volume)
+        const points = await res.json()
+        return parsePoints(points)
       } catch (e) {
         console.error(getErrorMessage(e))
         throw new Error()
       }
     },
     meta: {
-      error: "Unable to retrieve user volume data",
+      error: "Unable to retrieve user rank data",
+    },
+    enabled: !!address,
+    retry: false,
+    staleTime: 1 * 60 * 1000, // 1 minute
+  })
+}
+
+export function useUserBoosts() {
+  const { address } = useAccount()
+  return useQuery({
+    queryKey: ["user-boosts", address],
+    queryFn: async () => {
+      try {
+        if (!address) return null
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_MANGROVE_DATA_API_HOST}/incentives/boosts/${address}`,
+        )
+        const boosts = await res.json()
+        return parseBoosts(boosts)
+      } catch (e) {
+        console.error(getErrorMessage(e))
+        throw new Error()
+      }
+    },
+    meta: {
+      error: "Unable to retrieve user boosts data",
     },
     enabled: !!address,
     retry: false,
