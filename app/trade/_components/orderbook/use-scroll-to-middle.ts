1f1733aff0ec6b92ebab755c76ff31f8a4e67042
Johan le Roch <johanleroch@gmail.com>
2024-07-10 17:46:50 +0200

    Fix/eng 242 (#313)
    * disable auto scroll when the user has scrolled
    
    * eng-242: disable auto scroll when the user has scrolled

--- app/trade/_components/orderbook/use-scroll-to-middle.ts
+++ app/trade/_components/orderbook/use-scroll-to-middle.ts
@@ -1,12 +1,32 @@
+import useMarket from "@/providers/market.new"
 import React from "react"
 
 export default function useScrollToMiddle() {
+  const { currentMarket } = useMarket()
   const scrollAreaRef = React.useRef<HTMLDivElement>(null)
   const bodyRef = React.useRef<HTMLTableSectionElement>(null)
   const spreadRef = React.useRef<HTMLTableRowElement>(null)
+  const userScrolled = React.useRef(false)
+
+  // Enable auto scroll on market change
+  React.useEffect(() => {
+    userScrolled.current = false
+  }, [currentMarket?.base.address, currentMarket?.quote.address])
+
+  // Disable auto scroll on user scroll
+  React.useEffect(() => {
+    if (!scrollAreaRef.current) return
+    const handleScroll = () => {
+      userScrolled.current = true
+    }
+    scrollAreaRef.current?.addEventListener("wheel", handleScroll)
+    return () =>
+      scrollAreaRef.current?.removeEventListener("wheel", handleScroll)
+  }, [scrollAreaRef.current])
 
   React.useEffect(() => {
-    if (!scrollAreaRef.current || !bodyRef.current) return
+    if (!scrollAreaRef.current || !bodyRef.current || userScrolled.current)
+      return
     const scrollAreaRect = scrollAreaRef.current.getBoundingClientRect()
     const scrollableViewport = scrollAreaRef.current.querySelector(
       "div[data-radix-scroll-area-viewport]",
