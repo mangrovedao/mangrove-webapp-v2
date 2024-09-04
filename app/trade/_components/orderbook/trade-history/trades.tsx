be168bf5ac8f1848b543b3ad5ecec8582759fbcb
Johan le Roch <johanleroch@gmail.com>
2024-09-04 17:12:46 +0200

    End table and data

---
+++ app/trade/_components/orderbook/trade-history/trades.tsx
@@ -0,0 +1,46 @@
+"use client"
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
+import React from "react"
+
+import useMarket from "@/providers/market.new"
+import { useAccount } from "wagmi"
+import { DataTable } from "../../../../../components/ui/data-table/data-table"
+import { useTable } from "./use-table"
+import { useTradeHistory } from "./use-trade-history"
+
+export function Trades() {
+  const { chain } = useAccount()
+  const [{ page, pageSize }, setPageDetails] = React.useState<PageDetails>({
+    page: 1,
+    pageSize: 10,
+  })
+  const { currentMarket: market } = useMarket()
+  const tradesHistoryQuery = useTradeHistory({
+    filters: {
+      skip: (page - 1) * pageSize,
+    },
+  })
+
+  const table = useTable({
+    data: tradesHistoryQuery.data,
+  })
+
+  const blockExplorerUrl = chain?.blockExplorers?.default.url
+
+  return (
+    <ScrollArea className="h-full" scrollHideDelay={200}>
+      <DataTable
+        table={table}
+        isError={!!tradesHistoryQuery.error}
+        isLoading={tradesHistoryQuery.isLoading || !market}
+        onRowClick={(row) =>
+          row &&
+          row.transactionHash &&
+          window.open(`${blockExplorerUrl}/tx/${row.transactionHash}`, "_blank")
+        }
+        cellClasses="py-0 text-xs"
+      />
+      <ScrollBar orientation="vertical" className="z-50" />
+    </ScrollArea>
+  )
+}
--- components/ui/data-table/data-table.tsx
+++ components/ui/data-table/data-table.tsx
@@ -26,6 +26,7 @@ interface DataTableProps<TData> {
   renderExtraRow?: (row: Row<TData>) => React.ReactNode
   tableRowClasses?: string
   skeletonRows?: number
+  cellClasses?: string
 }
 
 export function DataTable<TData>({
@@ -39,6 +40,7 @@ export function DataTable<TData>({
   renderExtraRow = () => null,
   tableRowClasses,
   skeletonRows = 2,
+  cellClasses,
 }: DataTableProps<TData>) {
   const rows = table.getRowModel().rows
   const tableName = Object.keys(table._getAllFlatColumnsById()).join("-")
@@ -97,7 +99,10 @@ export function DataTable<TData>({
                   {row.getVisibleCells().map((cell) => (
                     <TableCell
                       key={`${tableName}-body-cell-${cell.id}`}
-                      className="px-0 py-2 group/cell whitespace-nowrap"
+                      className={cn(
+                        "px-0 py-2 group/cell whitespace-nowrap",
+                        cellClasses,
+                      )}
                     >
                       <div
                         className={cn(
