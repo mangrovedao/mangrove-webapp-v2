6e74d5a5a78f17fcf29fa6c1efb9851f409aeea2
Johan le Roch <johanleroch@gmail.com>
2024-10-28 17:17:25 +0100

    fix layout issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,33 +21,35 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="pl-[6.5rem] px-4">
-        <RootProvider>
-          <Sidebar />
-          <Navbar />
-          {children}
-          <AdminCommand />
-          <WrongNetworkAlertDialog />
-          <DisclaimerDialog />
-        </RootProvider>
-        <Toaster
-          position="bottom-center"
-          toastOptions={{
-            className: toastClasses,
-            classNames: {
-              toast: toastClasses,
-              title: titleClasses,
-              error: "!fill-red-100",
-              success: "!fill-green-caribbean",
-            },
-            style: {
-              fontFamily: "Axiforma",
-              fontSize: "14px",
-              border: "1px solid #032221",
-              borderRadius: "8px",
-            },
-          }}
-        />
+      <body className="">
+        <div className="pl-[6.5rem] px-4">
+          <RootProvider>
+            <Sidebar />
+            <Navbar />
+            {children}
+            <AdminCommand />
+            <WrongNetworkAlertDialog />
+            <DisclaimerDialog />
+          </RootProvider>
+          <Toaster
+            position="bottom-center"
+            toastOptions={{
+              className: toastClasses,
+              classNames: {
+                toast: toastClasses,
+                title: titleClasses,
+                error: "!fill-red-100",
+                success: "!fill-green-caribbean",
+              },
+              style: {
+                fontFamily: "Axiforma",
+                fontSize: "14px",
+                border: "1px solid #032221",
+                borderRadius: "8px",
+              },
+            }}
+          />
+        </div>
       </body>
     </html>
   )
