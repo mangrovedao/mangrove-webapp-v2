12eefe75257787882cfb68c063dfba3f18bc0b52
Johan le Roch <johanleroch@gmail.com>
2024-11-06 12:20:56 +0100

    WIP rive anim

--- app/more/components/quick-actions.tsx
+++ app/more/components/quick-actions.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button"
+import Rive, { Fit, Layout, useRive } from "@rive-app/react-canvas"
 import Link from "next/link"
 
 const ACTIONS = [
@@ -8,31 +9,46 @@ const ACTIONS = [
       "Take advantage of market volatility with your own instances of Kandel",
     href: "/strategies",
     imageUrl: "/assets/more/kandel.webp",
+    riveUrl: "/assets/rive/kandel.riv",
   },
   {
     title: "Bridge",
     description: "Send your assets accross chains, without leaving the app",
     href: "/bridge",
     imageUrl: "/assets/more/bridge.webp",
+    riveUrl: "/assets/rive/bridge.riv",
   },
   {
     title: "Wrap",
     description: "Wrap ETH into wETH, in order to trade ETH as an ERC20 token",
     href: "/wrap",
     imageUrl: "/assets/more/wrap.webp",
+    riveUrl: "/assets/rive/wrap.riv",
   },
 ]
 
 export default function QuickActions() {
   return (
     <div className="grid grid-cols-6 gap-4 mt-8">
+      <Anim />
       {ACTIONS.map((action) => (
-        <div className="w-full bg-bg-secondary rounded-2xl overflow-hidden col-span-6 sm:col-span-3 md:col-span-2">
-          <img
+        <div
+          className="w-full bg-bg-secondary rounded-2xl overflow-hidden col-span-6 sm:col-span-3 md:col-span-2"
+          key={action.title}
+        >
+          {/* <img
             src={action.imageUrl}
             alt="Kandel"
             className="object-cover scale-[1.02] w-full max-h-[200px] md:max-h-none"
-          />
+          /> */}
+          <div className="object-cover scale-[1.02] w-full h-96 max-h-[200px] md:max-h-none">
+            <Rive
+              className=""
+              layout={Layout.new({ fit: Fit.Cover })}
+              src={action.riveUrl}
+              stateMachines="Timeline 1"
+            />
+          </div>
           <div className="flex flex-col items-center text-center px-3 py-4 md:px-5 md:py-6">
             <h1 className="font-semibold text-lg md:text-[25px]">
               {action.title}
@@ -47,5 +63,19 @@ export default function QuickActions() {
         </div>
       ))}
     </div>
+  )
+}
+
+function Anim() {
+  const { rive, RiveComponent } = useRive({
+    src: "https://cdn.rive.app/animations/vehicles.riv",
+    stateMachines: "bumpy",
+    autoplay: false,
+  })
+  return (
+    <RiveComponent
+      onMouseEnter={() => rive && rive.play()}
+      onMouseLeave={() => rive && rive.pause()}
+    />
   )
 }
