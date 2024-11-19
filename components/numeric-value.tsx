9e7b0a37113e8b0efb4af9222e67fd7003ccd30b
belbazanas <belbazanas@gmail.com>
2024-11-19 13:33:17 +0100

    rewards: create component of approx value
    Signed-off-by: belbazanas <belbazanas@gmail.com>

---
+++ components/numeric-value.tsx
@@ -0,0 +1,21 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+
+export function NumericValue({ value }: { value: string }) {
+  const numericValue = Number(value)
+  const shouldShowApprox = numericValue <= 0.0001 && Number(value) !== 0
+  const displayValue = Number(value) > 0.0001 ? numericValue.toFixed(4) : "0"
+
+  return (
+    <>
+      {shouldShowApprox ? "~ " : ""}
+      <Tooltip>
+        <TooltipTrigger>{displayValue}</TooltipTrigger>
+        <TooltipContent>{value}</TooltipContent>
+      </Tooltip>
+    </>
+  )
+}
