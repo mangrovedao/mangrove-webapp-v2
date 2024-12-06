6692463f8062890228e95efbe72ab7ae18b80bd9
Anas <belbazanas@gmail.com>
2024-12-06 18:11:19 +0100

    Deploy hide null trades in history fix 🚀 (#398)
    * vault: rename APR into APY
    
    Signed-off-by: belbazanas <belbazanas@gmail.com>
    
    * vault: update APR into APY
    
    Signed-off-by: belbazanas <belbazanas@gmail.com>
    
    * add base sepolia to staging
    
    Signed-off-by: belbazanas <belbazanas@gmail.com>
    
    * network: revert last changes
    
    Signed-off-by: belbazanas <belbazanas@gmail.com>
    
    * network: revert last changes
    
    Signed-off-by: belbazanas <belbazanas@gmail.com>
    
    * vault: update APR to APY (#393)
    
    Signed-off-by: belbazanas <belbazanas@gmail.com>
    
    * trade: review success toast duration (#395)
    
    Signed-off-by: belbazanas <belbazanas@gmail.com>
    
    * ENG-410: Create a filter for zero trade history (#397)
    
    * clean: update indexerSDk getPrice() with price.mgvinfra.com endpoint
    
    Signed-off-by: belbazanas <belbazanas@gmail.com>
    
    ---------
    
    Signed-off-by: belbazanas <belbazanas@gmail.com>
    Co-authored-by: Enric Borrallo <borrallorodriguez@gmail.com>

--- providers/mangrove.tsx
+++ providers/mangrove.tsx
@@ -2,38 +2,7 @@
 
 import React from "react"
 
-const useMangroveContext = () => {
-  // const signer = useEthersSigner()
-  // const { isConnected, chain } = useAccount()
-  // const { chains } = useConfig()
-  // const tokenPricesInUsbQuery = useTokenPricesInUsb()
-  // const isNetworkSupported = chains.find((c) => c.id === chain?.id)
-  // console.log(chain?.id)
-  // const mangroveQuery = useQuery({
-  //   queryKey: ["mangroveInstance", signer?._address, chain?.id],
-  //   queryFn: async () => {
-  //     try {
-  //       const mangrove = await Mangrove.connect({ signer })
-  //       // overwrite the mangrove's configuration thanks to the mangroveConfig env variable
-  //       if (mangroveConfig) {
-  //         mangrove.updateConfiguration(mangroveConfig)
-  //       }
-  //       return mangrove
-  //     } catch (e) {
-  //       const message = getErrorMessage(e)
-  //       console.error(message)
-  //       throw new Error(message)
-  //     }
-  //   },
-  //   enabled: !!signer?._address && !!isConnected && !!isNetworkSupported,
-  //   refetchOnWindowFocus: false,
-  //   retry: false,
-  //   staleTime: 15 * 60 * 1000,
-  // })
-  // const { data: mangrove } = mangroveQuery
-  // const marketsInfoQuery = useWhitelistedMarketsInfos(mangrove)
-  // return { mangroveQuery, mangrove, marketsInfoQuery, tokenPricesInUsbQuery }
-}
+const useMangroveContext = () => {}
 
 const MangroveContext = React.createContext<
   ReturnType<typeof useMangroveContext> | undefined
