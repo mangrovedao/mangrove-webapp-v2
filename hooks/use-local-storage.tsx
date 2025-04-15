8023f0cb6b536b70301a3507a50d873838300ad0
George <georgepat2000@gmail.com>
2025-04-15 19:42:59 +0400

    fix: localstorage checks

--- hooks/use-local-storage.tsx
+++ hooks/use-local-storage.tsx
@@ -1,15 +1,14 @@
 import { useState } from "react"
 
 function useLocalStorage<T>(key: string, initialValue: T) {
-  if (typeof window === "undefined") {
-    return [initialValue, () => {}]
-  }
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState<T>(() => {
     try {
       // Get from local storage by key
-      const item = window.localStorage.getItem(key)
+      const item =
+        typeof window === "undefined" &&
+        (window as Window).localStorage.getItem(key)
       // Parse stored json or if none return initialValue
       return item ? JSON.parse(item) : initialValue
     } catch (error) {
@@ -28,7 +27,11 @@ function useLocalStorage<T>(key: string, initialValue: T) {
       // Save state
       setStoredValue(valueToStore)
       // Save to local storage
-      window.localStorage.setItem(key, JSON.stringify(valueToStore))
+      typeof window === "undefined" &&
+        (window as Window).localStorage.setItem(
+          key,
+          JSON.stringify(valueToStore),
+        )
     } catch (error) {
       // A more advanced implementation would handle the error case
       console.error(error)
