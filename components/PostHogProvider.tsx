863260ea69059c9f33345086d7110f9f0030f2e6
belbazanas <belbazanas@gmail.com>
2025-04-17 12:44:24 +0200

    test(posthog-setup): try new posthog init params
    Signed-off-by: belbazanas <belbazanas@gmail.com>

--- components/PostHogProvider.tsx
+++ components/PostHogProvider.tsx
@@ -8,7 +8,8 @@ import { Suspense, useEffect } from "react"
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      api_host: "https://staging.mangrove.exchange/ingest",
+      ui_host: process.env.NEXT_PUBLIC_POSTHOG_UI_HOST,
       capture_pageview: false, // We capture pageviews manually
       capture_pageleave: true, // Enable pageleave capture
     })
