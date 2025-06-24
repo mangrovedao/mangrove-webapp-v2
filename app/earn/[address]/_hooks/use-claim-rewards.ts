5b2687490600906819f62daedaa34d6d9f1dad8d
Anas <48021899+anasbelbaz@users.noreply.github.com>
2025-06-24 11:28:19 +0200

    claim rewards update (#532)
    * chore: remove evm errors from toast
    
    Signed-off-by: belbazanas <belbazanas@gmail.com>
    
    * fix: remove gas limit
    
    Signed-off-by: belbazanas <belbazanas@gmail.com>
    
    * fix: vault claim rewards
    
    Signed-off-by: belbazanas <belbazanas@gmail.com>
    
    * feat: add sucess toast on rewards claim
    
    Signed-off-by: belbazanas <belbazanas@gmail.com>
    
    * feat: add sucess toast on rewards claim
    
    Signed-off-by: belbazanas <belbazanas@gmail.com>
    
    * feat: add sucess toast on rewards claim
    
    Signed-off-by: belbazanas <belbazanas@gmail.com>
    
    ---------
    
    Signed-off-by: belbazanas <belbazanas@gmail.com>

--- app/earn/[address]/_hooks/use-claim-rewards.ts
+++ app/earn/[address]/_hooks/use-claim-rewards.ts
@@ -2,24 +2,14 @@ import { useDefaultChain } from "@/hooks/use-default-chain"
 import { useNetworkClient } from "@/hooks/use-network-client"
 import { printEvmError } from "@/utils/errors"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
-import { Address, parseUnits } from "viem"
+import { toast } from "sonner"
+import { Address, parseAbi } from "viem"
 import { simulateContract, waitForTransactionReceipt } from "viem/actions"
 import { useAccount, useWalletClient } from "wagmi"
 
-const rewardsAbi = [
-  {
-    inputs: [
-      { name: "account", type: "address" },
-      { name: "reward", type: "address" },
-      { name: "amount", type: "uint256" },
-      { name: "proof", type: "bytes32[]" },
-    ],
-    name: "claim",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-] as const
+const rewardsAbi = parseAbi([
+  "function claim(address account, address reward, uint256 amount, bytes32[] proof)",
+])
 
 const rewardDistributorAddress = "0xDb6A3A20743f5878732EF73623a51033c80DBB10"
 
@@ -37,7 +27,7 @@ export function useClaimRewards() {
       proof,
     }: {
       rewardToken: Address
-      amount: number
+      amount: string
       proof: `0x${string}`[]
     }) => {
       try {
@@ -47,33 +37,27 @@ export function useClaimRewards() {
           address: rewardDistributorAddress,
           abi: rewardsAbi,
           functionName: "claim",
-          args: [
-            address,
-            rewardToken,
-            parseUnits(amount.toString(), 18),
-            proof,
-          ],
+          args: [address, rewardToken, BigInt(amount), proof],
         })
 
         const tx = await walletClient?.writeContract(request)
         if (!tx) throw new Error("Transaction failed")
         const receipt = await waitForTransactionReceipt(client, { hash: tx })
         if (receipt.status === "success") {
+          toast.success("Rewards claimed successfully")
           return receipt
         } else {
-          throw new Error("Transaction failed")
+          throw new Error(receipt.status)
         }
       } catch (error) {
         printEvmError(error)
+        toast.error("Transaction failed")
       }
     },
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ["rewards-proofs", address, defaultChain.id],
       })
-    },
-    onError: (error) => {
-      printEvmError(error)
     },
   })
 }
